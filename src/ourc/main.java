7306a6b836e44519ac2b30caf9d11c6ea0b00b09
李順平 10920131 <10920131@O365st.cycu.edu.tw>
2024-03-11 22:42:59 +0800

    change main

--- src/ourc/main.java
+++ src/ourc/main.java
@@ -1,13 +1,11 @@
 package ourc;
 
 
-
-class Test{  // 注意類別名稱需要跟.java檔名相同
+class main{  // 注意類別名稱需要跟.java檔名相同
     public static void main(String[] args){
         System.out.println("Hello Java~~");
     }
 }
-
 
 
 
