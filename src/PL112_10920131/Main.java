0e94dc3a0cc2ab3a26f75f13f62c4f66724c2a75
李順平 10920131 <10920131@O365st.cycu.edu.tw>
2024-03-12 13:41:04 +0800

    add struct

--- src/PL112_10920131/Main.java
+++ src/PL112_10920131/Main.java
@@ -1,12 +1,26 @@
 package PL112_10920131;
 
+class Main { // 注意類別名稱需要跟.java檔名相同
 
-class Main{  // 注意類別名稱需要跟.java檔名相同
-    public static void main(String[] args){
-        System.out.println("Hello Java~~");
-    }
+	static class ListNode {
+		int type;
+		String item;
+		ListNode next;
+
+		ListNode(int type, String item) {
+			this.type = type;
+			this.item = item;
+			this.next = null;
+		}
+	}
+
+	private ListNode commandhead ;
+	
+
+	
+	
+
+	public static void main(String[] args) {
+		System.out.println("Hello Java~~");
+	}
 }
-
-
-
-
